3cd3691fd3403a867c30c097c07502a9f4e34767
Yulianna-B <juliburkovska@gmail.com>
2025-06-28 01:13:50 +0200

    Homework 11 is done

---
+++ consultation/js-intro/index.js
@@ -0,0 +1,31 @@
+  console.log("hello world")
+
+  // создание переменых const, let, var
+
+  let dog = "Bob"
+  dog = "Sharik"
+  console.log(dog)
+
+  let bird;
+  bird="Woody";
+  console.log(bird);
+
+  const car ="Porsche"
+  console.log(car);
+
+// var - имеет глобальную видимость ее видно внутри любого блока
+  var age = 35;
+
+  // Примитивные типы данных 
+  // 1.String
+  
+  let str1= "можно в двойных ковычках"
+  let str2= "можно в одинарних ковычках"
+
+   let str3= "13" //this is a string 
+   let str4= str1 + " " + str2;
+console.log(str1.toUpperCase);
+
+
+   console.log(str4);
+   
