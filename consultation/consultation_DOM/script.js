f0058eb9a1f85c6b8aec72a0bd84c884551ce5ce
Yulianna-B <juliburkovska@gmail.com>
2025-07-08 01:14:28 +0200

    consultation_DOM is done

--- consultation/consultation_DOM/script.js
+++ consultation/consultation_DOM/script.js
@@ -5,3 +5,56 @@ const listElement = document.getElementById('person-list')
 const clearBtnElement = document.getElementById('clear')
 const clearOneBtnElement =document.getElementById('clear-one')
 
+function clearList() {
+    while (listElement.firstChild) {
+        listElement.removeChild(listElement.firstChild);
+}
+persons.length= 0;
+
+}
+
+function clearOne() {
+    if (listElement.firstChild) {
+        listElement.removeChild(listElement.firstChild);
+        persons.shift();
+
+    }
+}
+
+function clearInputs(event) {
+    event.target.nickname.value = '';
+    event.target.place.value = '';
+}
+
+
+function changeStatus(event) {
+    const li = event.target;
+    if (li.style.textDecoration === 'line-through') {
+        li.style.textDecoration = 'none'
+    } else {
+        li.style.textDecoration = 'line-through'
+    }
+}
+
+formElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+const person = {
+    name: event.target.nickname.value,
+    place: event.target.place.value
+ };
+
+ persons.push(person);
+
+ const li = document.createElement('li');
+ li.textContent = `${person.name} ${person.place}🥷🏼`;
+ li.addEventListener('click', changeStatus);
+
+listElement.appendChild(li);
+clearInputs(event);
+
+});
+
+clearBtnElement.addEventListener('click', clearList);
+clearOneBtnElement.addEventListener('click', clearOne);
